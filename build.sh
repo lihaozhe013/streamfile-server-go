9e3f747f515101493e12d7088c75bc46e1408a34
Haozhe Li <lihaozhe013@outlook.com>
2025-08-11 13:58:41 +0800

    chore: small changes
    1. removed chinese comments
    2. fixed bash script error

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 cd react
 npm install
 npm run build
-rm -r ../public/markdown-viewer
+[ -d ../public/markdown-viewer ] && rm -r ../public/markdown-viewer
 mkdir -p ../public/markdown-viewer
 cp -r dist/* ../public/markdown-viewer/
 
