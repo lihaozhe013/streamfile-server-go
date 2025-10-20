e0c4af0c5d8dbe08508230b2d18f4926ad8b1a09
Haozhe Li <lihaozhe013@outlook.com>
2025-10-20 10:57:46 -0400

    feat: build script based on node.js

---
+++ scripts/post-build-prod.js
@@ -0,0 +1,23 @@
+const fs = require("fs-extra");
+const path = require("path");
+const root = path.resolve(__dirname, "..");
+const r = (...p) => path.resolve(root, ...p);
+
+fs.rmSync(r("dist"), { recursive: true });
+fs.ensureDirSync(r("dist"));
+fs.copySync(r("public"), r("dist/public"))
+fs.copySync(r("config.yaml.example"), r("dist/config.yaml.example"))
+try {
+    fs.copySync(r("config.yaml"), r("dist/config.yaml"))
+}
+catch {}
+
+try {
+    fs.copySync(r("simple-server"), r("dist/simple-server"))
+}
+catch {}
+
+try {
+    fs.copySync(r("simple-server.exe"), r("dist/simple-server.exe"))
+}
+catch {}
