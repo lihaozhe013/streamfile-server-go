52986aef7bc892395140067c225c7585663b098e
Haozhe Li <lihaozhe013@outlook.com>
2025-10-21 09:29:07 -0400

    refactor: build script enhencement

--- scripts/clean.js
+++ scripts/clean.js
@@ -5,21 +5,25 @@ const r = (...p) => path.resolve(root, ...p);
 
 try {
     fs.rmSync(r("simple-server"));
+    console.log("removed /simple-server");
 }
 catch {}
 
 try {
     fs.rmSync(r("simple-server.exe"));
+    console.log("removed /simple-server.exe");
 }
 catch {}
 
 try {
     fs.rmSync(r("public/markdown-viewer"), { recursive: true });
+    console.log("removed public/markdown-viewer");
 }
 catch {}
 
 try {
     fs.rmSync(r("public/styles.css"));
+    console.log("removed public/styles.css");
 }
 catch {}
 
--- scripts/post-build-prod.js
+++ scripts/post-build-prod.js
@@ -10,18 +10,24 @@ catch {}
 
 fs.ensureDirSync(r("dist"));
 fs.copySync(r("public"), r("dist/public"))
+console.log("/public/ moved to /dist/public");
+
 fs.copySync(r("config.yaml.example"), r("dist/config.yaml.example"))
+console.log("/config.yaml.example moved to /dist/config.yaml.example");
 try {
     fs.copySync(r("config.yaml"), r("dist/config.yaml"))
+    console.log("/config.yaml moved to /dist/config.yaml");
 }
 catch {}
 
 try {
     fs.copySync(r("simple-server"), r("dist/simple-server"))
+    console.log("/simple-server moved to /dist/simple-server");
 }
 catch {}
 
 try {
     fs.copySync(r("simple-server.exe"), r("dist/simple-server.exe"))
+    console.log("/simple-server.exe moved to /dist/simple-server.exe");
 }
 catch {}
